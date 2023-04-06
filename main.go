f139a13e72aba47dc4ab380ecc25939729ce54ae
Anju Balakrishnan <anjoos2006@gmail.com>
2023-04-06 23:30:15 +0530

    loop

--- main.go
+++ main.go
@@ -3,69 +3,80 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	// Variable declaration
-	var conferenceName = "Go Conference"
+	// Variable declaration is like var conferenceName = "Go Conference"
+
+	// Alternative syntax for defining a variable and assigning it a value
+	// Just a syntactic sugar
+	// Only applies to var and not const. And it also doesnt work, if you  want to include the type 
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
+	
 	var remainingTickets uint  = 50
 	// fmt function is used to accept or print 
 	// println is used for printing a line
 	fmt.Println("Welcome to", conferenceName, "booking application!")
 	fmt.Println("We have a total of",conferenceTickets,"tickets and", remainingTickets, "are available.")
 	fmt.Println("Get your tickets here to attend.")
-	//  Array declaration
-	var bookings [50] string
-
+	
 	// slices
 	// Abstraction of an array. i.e. variable length array
 	// It can get a sub-array of its own
 	//  Slices are also index-based and have a size but its resized when needed
-	var bookingList []string
+	// var bookingList []string
+	bookingList := []string {}
+	for {
+		// Variable declaration without initialising and we need to specify the datatype if its not initialised
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// Variable declaration without initialising and we need to specify the datatype if its not initialised
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+		println("Enter your first name")
+		// scan is used to accept the user input
+		fmt.Scan(&firstName)
 
-	println("Enter your first name")
-	// scan is used to accept the user input
-	fmt.Scan(&firstName)
+		println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	println("Enter your last name")
-	fmt.Scan(&lastName)
+		println("Enter your Email")
+		fmt.Scan(&email)
 
-	println("Enter your Email")
-	fmt.Scan(&email)
+		println("Enter the number of tickets")
+		fmt.Scan (&userTickets)
+		
+		if remainingTickets < userTickets {
+			fmt.Printf("We only have %v tickets remaining, so you cant book %v tickets", remainingTickets, userTickets)
+			break
+		}
 
-	println("Enter the number of tickets")
-	fmt.Scan (&userTickets)
-	// Adding value to array
-	bookings[0] = firstName + " " + lastName
+		// Adding value to the slice
+		//  We dont need to know the current index.use the append
+		bookingList = append(bookingList, firstName + " " + lastName)
+		remainingTickets = remainingTickets - userTickets
 
-	// Adding value to the size
-	//  We dont need to know the current index.use the append
-	bookingList = append(bookingList, firstName + " " + lastName)
-	remainingTickets = remainingTickets - userTickets
+		fmt.Printf("Thank you %v %v for booking %v tickets.You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining", remainingTickets)
 
+		firstNames := []string{}
 
-	// %v indicates the value
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v \n", bookings[0])
-	// %T is used to display the Type
-	fmt.Printf("Array Type: %T \n", bookings)
-	fmt.Printf("Array length: %v \n", len(bookings))
+		// for index, booking := range bookingList
+		// _ is a Blank Identifier in Go
+		// To ignor a variable that you dont want to use
+		// i.e. with Go, we need to make unused variables explicit
+		for _, booking := range bookingList {
+			// splitting a string by spaces
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
-	// %v indicates the value
-	fmt.Printf("The whole slice: %v\n", bookingList)
-	fmt.Printf("The first value: %v \n", bookingList[0])
-	// %T is used to display the Type
-	fmt.Printf("Slice Type: %T \n", bookingList)
-	fmt.Printf("Slice length: %v \n", len(bookingList))
-
-
-	fmt.Printf("Thank you %v %v for booking %v tickets.You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining", remainingTickets)
+		if remainingTickets == 0 {
+			fmt.Println("Our Conference is booked. Please come back later!")	
+			break
+		}
+	}
 }
