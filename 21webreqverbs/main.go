86e8b93f0a670ad87a4c521447677203476ab643
Anju Balakrishnan <anjoos2006@gmail.com>
2023-04-11 14:11:20 +0530

    Http Post request in go with json data

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -8,8 +8,10 @@ import (
 )
 
 func main() {
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
+// Function which performs the get request
 func PerformGetRequest() {
 	const myurl = "http://localhost:8000/get"
 	response, err := http.Get(myurl)
@@ -29,4 +31,25 @@ func PerformGetRequest() {
 	fmt.Println("response: ",responseString.String())
 	// fmt.Println(string(content))
 	// Content is in the byte format
+}
+
+func PerformPostJsonRequest(){
+	const myurl = "http://localhost:8000/post"
+
+	// Fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with golang",
+			"price": 0,
+			"platform":"learnCodeOnline.in"
+		}
+	`)
+	response, err := http.Post(myurl, "application/json",requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
 }
